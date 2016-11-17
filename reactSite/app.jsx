50554d49023db648898f7755a9fa1ca2404c63ee
kampana <urishmueli@gmail.com>
2016-11-17 15:11:49 +0200

    Starting the layout

--- reactSite/app.jsx
+++ reactSite/app.jsx
@@ -1,34 +1,34 @@
 import React from 'react';
 import ContentBox from './components/ContentBox.jsx'
 
-export default class App extends React.Component {
+        export default class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {contentBoxClicked: 'hello to me'};
         this.contentBoxesHTML = [];
         this.contentBoxes = [];
         this.contentBoxesInformation = [{
-                title: 'abc',
-                content: ['2016 - Present: Panaya', '2008-2016: IBM']
+                title: '2016 - Present: Full Stack Developer',
+                content: ['JAVA', 'Angular','Spring / Hibernate', 'C#']
             },
             {
-                title: 'def',
-                content: ['2016 - Present: Panaya', '2008-2016: IBM']
+                title: '2013 - 2016: Team Lead at IBM - XIV',
+                content: ['Angular', 'JAVA']
             },
             {
-                title: 'rrr',
-                content: ['2016 - Present: Panaya', '2008-2016: IBM']
+                title: '2010 - 2013: Team Lead at IBM - Diligent',
+                content: ['JAVA', 'SWING']
             },
             {
-                title: 'ccc',
-                content: ['2016 - Present: Panaya', '2008-2016: IBM']
+                title: '2008 - 2010: GUI Developer at IBM - Diligent',
+                content: ['JAVA', 'SWING']
             }
         ];
         this.contentBoxesInformation.forEach(c => {
             const contentBoxHTML = <ContentBox ref={(c) => this.contentBoxes.push(c) }  onClick={this.handleClick.bind(this)} content={c.content} title={c.title} ></ContentBox>
             this.contentBoxesHTML.push(contentBoxHTML);
         });
-        
+
         this.counter = 0;
     }
 
@@ -41,9 +41,22 @@ export default class App extends React.Component {
     render() {
 
         return (
-                <div id="centerThem">
-                    {this.contentBoxesHTML}
+                <div>
+                    <div style={{fontSize: '200%'}}>Uri Shmueli
+                    </div>
+                    <div style={{marginTop: '0.5em', marginBottom: '2em'}}>
+                        Phone information<br></br>
+                        Mail information                
+                    </div>
+                    <div>
+                    <div style={{fontSize: '150%', borderStyle: 'none none solid none', marginBottom: '1em'}}>Exprience
+                    </div>
+                        <div>
+                            {this.contentBoxesHTML}
+                        </div>
+                
+                    </div>
                 </div>
-                );
-    }
-}
+                                );
+                    }
+                }
