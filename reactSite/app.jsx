23f45da951ba05612e63bf92fac8679e7a7775e3
kampana <urishmueli@gmail.com>
2016-12-11 21:23:00 +0200

    ComponentDidUpdate and componentWillReceiveProps

--- reactSite/app.jsx
+++ reactSite/app.jsx
@@ -25,7 +25,6 @@ export default class App extends React.Component {
     }
 
     render() {
-        console.log(this.props.contentBoxesInformation);
         return (
                 <div>
                     <CVPersonalInformation></CVPersonalInformation>
--- reactSite/components/CVExprience.jsx
+++ reactSite/components/CVExprience.jsx
@@ -5,20 +5,27 @@ import ContentBox from './contentBox/ContentBox.jsx';
 export default class CVExprience extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            contentBoxesHTML : []
+        }
+    }
 
+    componentWillReceiveProps(nextProps) {
         this.contentBoxesHTML = [];
         this.contentBoxes = [];
-        this.props.content.forEach(c => {
+        nextProps.content.forEach(c => {
             const contentBoxHTML = 
             <ContentBox key={c.years} ref={(c) => this.contentBoxes.push(c) }  onClick={this.handleClick.bind(this)} 
             content={c.content} imageName={c.imageName} years={c.years} title={c.title} ></ContentBox>
             this.contentBoxesHTML.push(contentBoxHTML);
         });
 
+
+        this.setState( {contentBoxesHTML : this.contentBoxesHTML });
     }
 
-    componentDidMount() {
-        // Open the first exprience
+    componentDidUpdate(prevProps, prevState) {
+         // Open the first exprience
         if (this.contentBoxes[0]) {
             this.contentBoxes[0].eventTriggered(this.contentBoxes[0]);
         }
@@ -33,7 +40,7 @@ export default class CVExprience extends React.Component {
     render() {
         return (
                 <div>
-                    {this.contentBoxesHTML}
+                    {this.state.contentBoxesHTML}
                 </div>
                 )
     }
