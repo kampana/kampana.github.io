d7c04101f833c811ca4c2176a44224c55022ca85
kampana <urishmueli@gmail.com>
2016-12-11 21:24:06 +0200

    Publish

--- reactSite/webpack.production.config.js
+++ reactSite/webpack.production.config.js
@@ -9,7 +9,8 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
-          presets: ['es2015', 'react']
+          presets: ['es2015', 'react'],
+          plugins: ['transform-decorators-legacy']
         }
       }
     ]
