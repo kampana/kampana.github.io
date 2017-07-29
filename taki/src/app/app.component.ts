e964a05d3e369aa29e4b9daecbae6236cdf0f808
kampana <urishmueli@gmail.com>
2017-07-29 22:57:54 +0300

    TakiLogin 2

--- taki/src/app/app.component.ts
+++ taki/src/app/app.component.ts
@@ -1,30 +1,43 @@
-import { Component, AfterViewInit, AfterContentChecked, AfterViewChecked, AfterContentInit } from '@angular/core';
+import { DeckFetcherService } from './services/deck-fetcher.service';
+import { DropVerificationService } from './services/drop-verification.service';
+import { HttpClient } from '@angular/common/http';
+import { Component, AfterViewInit, AfterContentChecked, AfterViewChecked, AfterContentInit, OnInit } from '@angular/core';
 import { MdDialog } from "@angular/material";
 import { LoginDialogComponent } from "app/login-dialog/login-dialog.component";
-import { DropVerificationService } from "app/services/drop-verification.service";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [DropVerificationService]
+  providers: [DropVerificationService, DeckFetcherService]
 })
-export class AppComponent implements AfterViewInit, AfterContentInit {
+export class AppComponent implements AfterViewInit, AfterContentInit, OnInit {
+  returnedData = "waiting";
+
+  constructor(private dialog: MdDialog,
+    private dropVerificationService: DropVerificationService,
+    private deckFetcherService: DeckFetcherService) {
+  }
+
+  ngOnInit(): void {
+    let myCards = this.deckFetcherService.doLogin(data => {
+      //this.returnedData = data[0].value;
+      this.returnedData = data;
+    });
+  }
 
   ngAfterContentInit(): void {
   }
 
-  constructor(public dialog: MdDialog, private dropVerificationService: DropVerificationService) {
-    dropVerificationService.doSomething();
-  }
 
   ngAfterViewInit(): void {
-   /*  setTimeout(() => {
-      this.dialog.open(LoginDialogComponent, {
-        height: '18em',
-        width: '30%',
-        disableClose: true,
-      });
-    }, 500); */
+    /*  setTimeout(() => {
+       this.dialog.open(LoginDialogComponent, {
+         height: '18em',
+         width: '30%',
+         disableClose: true,
+       });
+     }, 500); */
   }
 }
--- taki/src/app/services/drop-verification.service.spec.ts
+++ taki/src/app/services/drop-verification.service.spec.ts
@@ -1,3 +1,4 @@
+import { CardColor } from 'app/model/cardColor';
 import { TestBed, inject } from '@angular/core/testing';
 
 import { DropVerificationService } from './drop-verification.service';
@@ -14,11 +15,47 @@ describe('DropVerificationService', () => {
     expect(service).toBeTruthy();
   }));
 
-  it('should drop card blue on card blue', inject([DropVerificationService], (service: DropVerificationService) => {
+  it('should be able to drop color X on the same card color X', inject([DropVerificationService], (service: DropVerificationService) => {
     let myCard = new Card();
-    myCard.
+    myCard.cardColor = CardColor.Blue;
     let cardOnDeck = new Card();
+    cardOnDeck.cardColor = CardColor.Blue;
 
     expect(service.isDroppable(cardOnDeck, myCard)).toBe(true);
   }));
+
+    it('should be able to drop color X on the same card color X. with a different value', inject([DropVerificationService], (service: DropVerificationService) => {
+    let myCard = new Card();
+    myCard.cardColor = CardColor.Blue;
+    myCard.value = 7;
+    let cardOnDeck = new Card();
+    cardOnDeck.cardColor = CardColor.Blue;
+    cardOnDeck.value = 5;
+
+    expect(service.isDroppable(cardOnDeck, myCard)).toBe(true);
+  }));
+
+    it('should be able to drop color X on the color Y. with the same value', inject([DropVerificationService], (service: DropVerificationService) => {
+    let myCard = new Card();
+    myCard.cardColor = CardColor.Blue;
+    myCard.value = 7;
+    let cardOnDeck = new Card();
+    cardOnDeck.cardColor = CardColor.Green;
+    cardOnDeck.value = 7;
+
+    expect(service.isDroppable(cardOnDeck, myCard)).toBe(true);
+  }));
+
+     it('should not be able to drop color X on the color Y, different value', inject([DropVerificationService], (service: DropVerificationService) => {
+    let myCard = new Card();
+    myCard.cardColor = CardColor.Blue;
+    myCard.value = 8;
+    let cardOnDeck = new Card();
+    cardOnDeck.cardColor = CardColor.Green;
+    cardOnDeck.value = 7;
+
+    expect(service.isDroppable(cardOnDeck, myCard)).toBe(false);
+  }));
+
+
 });
